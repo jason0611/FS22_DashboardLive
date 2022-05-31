d57be6313bc0120870ff5c6dd951871321c19e17
Martin Eller <martin@eller-essen.de>
2022-05-31 17:45:35 +0200

    Update RegisterDashboardLive.lua

--- RegisterDashboardLive.lua
+++ RegisterDashboardLive.lua
@@ -5,19 +5,25 @@
 -- Version 0.1.0.1
 --
 
+source(g_currentModDirectory.."tools/gmsDebug.lua")
+GMSDebug:init(g_currentModName, true, 4)
+GMSDebug:enableConsoleCommands()
+
+local specName = g_currentModName..".DashboardLive"
+
 if g_specializationManager:getSpecializationByName("DashboardLive") == nil then
   	g_specializationManager:addSpecialization("DashboardLive", "DashboardLive", g_currentModDirectory.."DashboardLive.lua", nil)
+  	dbgprint("Specialization 'DashboardLive' added", 2)
 end
 
-for typeName, typeEntry in pairs(g_vehicleTypeManager:types) do
+for typeName, typeEntry in pairs(g_vehicleTypeManager.types) do
     if
 		SpecializationUtil.hasSpecialization(Dashboard, typeEntry.specializations)
 		and
 		SpecializationUtil.hasSpecialization(Motorized, typeEntry.specializations)
     then
-     	g_vehicleTypeManager:addSpecialization(typeName, "DashboardLive")
-     	--g_vehicleTypeManager:addSpecialization(typeEntry.name, "FS22_DashboardLive.DashboardLive")
-     	print("Added to "..tostring(typeName))
+     	g_vehicleTypeManager:addSpecialization(typeName, specName)
+		dbgprint(specName.." registered for "..typeName)
     end
 end
 
